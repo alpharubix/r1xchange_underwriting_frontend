2f6735de453f0d11e316a2045995e7920e9f0854
SaranDhanabal <stivensarab@gmail.com>
2026-08-06 17:33:47 +0530

    fix:build error

--- src/components/gst/Step2BusinessInfo.tsx
+++ src/components/gst/Step2BusinessInfo.tsx
@@ -13,7 +13,7 @@ interface Step2Props {
 
 const GSTIN_REGEX = /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/;
 
-export default function Step2BusinessInfo({ gstin, onSuccessSubmit, onRequiresAuth, onBack, onGstinChange }: Step2Props) {
+export default function Step2BusinessInfo({ gstin, onSuccessSubmit, onRequiresAuth,onGstinChange }: Step2Props) {
   const [fromMonth, setFromMonth] = useState("");
   const [toMonth, setToMonth] = useState("");
   const [needsAuth, setNeedsAuth] = useState(false);
