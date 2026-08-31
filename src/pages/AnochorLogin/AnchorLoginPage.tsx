252a4ee3d2ee809949ae5c34fda7a1df1ee428d3
PrathamPai2004 <prathameshpai.sdmcet26@gmail.com>
2026-08-31 18:49:50 +0530

    refix : deloyment issues | React not defined

--- src/pages/AnochorLogin/AnchorLoginPage.tsx
+++ src/pages/AnochorLogin/AnchorLoginPage.tsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardContent } from "@/components/ui/card";
-import { regex, z } from "zod";
+import { z } from "zod";
 import { useAnchorLogin, getApiError } from "@/hooks/useAuth";
 import { toast } from "sonner";
 import r1xchangeLogoWhiteWebView from "@/assets/r1xchangeLogoWhiteWebView.svg";
